625c72d3d2141fb5ea59d3282422b426412e332b
lk13227 <763149849@qq.com>
2018-05-21 17:55:00 +0800

    转换度分秒

--- PoiDemo/ViewController.m
+++ PoiDemo/ViewController.m
@@ -38,8 +38,12 @@
         //只打印一次经纬度
         NSLog(@"lat lng (%f, %f)", lat, lng);
         
+        // 转换度分秒
+        NSString *latStr = [self stringWithCoordinateString:lat];
+        NSString *lngStr = [self stringWithCoordinateString:lng];
+        
         // 显示label
-        self.locationLabel.text = [NSString stringWithFormat:@"纬度 经度 (%f, %f)", lat, lng];
+        self.locationLabel.text = [NSString stringWithFormat:@"纬度 经度 (%@, %@)", latStr, lngStr];
         
         if (!isOnece) {
             [U3dLocationManager stop];
@@ -68,5 +72,39 @@
             [U3dLocationManager stop];
         }
     }];
+}
+
+//经纬度转换度分秒
+/** 经纬度转换成度分秒格式 */
+- (NSString *)stringWithCoordinateString:(double )coordinate
+{
+    NSString *coordinateString = [NSString stringWithFormat:@"%f", coordinate];
+    /** 将经度或纬度整数部分提取出来 */
+    int latNumber = [coordinateString intValue];
+    
+    /** 取出小数点后面两位(为转化成'分'做准备) */
+    NSArray *array = [coordinateString componentsSeparatedByString:@"."];
+    /** 小数点后面部分 */
+    NSString *lastCompnetString = [array lastObject];
+    
+    /** 拼接字字符串(将字符串转化为0.xxxx形式) */
+    NSString *str1 = [NSString stringWithFormat:@"0.%@", lastCompnetString];
+    
+    /** 将字符串转换成float类型以便计算 */
+    float minuteNum = [str1 floatValue];
+    
+    /** 将小数点后数字转化为'分'(minuteNum * 60) */
+    float minuteNum1 = minuteNum * 60;
+    
+    /** 将转化后的float类型转化为字符串类型 */
+    NSString *latStr = [NSString stringWithFormat:@"%f", minuteNum1];
+    
+    /** 取整数部分即为纬度或经度'分' */
+    int latMinute = [latStr intValue];
+    
+    /** 将经度或纬度字符串合并为(xx°xx')形式 */
+    NSString *string = [NSString stringWithFormat:@"%d°%d'", latNumber, latMinute];
+    
+    return string;
 }
 @end
